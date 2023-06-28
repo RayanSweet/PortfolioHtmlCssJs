dc12173d072a220280b55f5deeb61335d712e99d
Sweets <rayan.telli@outlook.fr>
2023-06-28 18:04:30 +0200

    pop up update

--- js/popup.js
+++ js/popup.js
@@ -2,3 +2,23 @@ function togglePopupComprendre(){
     let popup = document.querySelector("#popup-overlay");
     popup.classList.toggle("open");
 }
+
+function togglePopupConcevoir(){
+    let popup = document.querySelector("#popup-overlay2");
+    popup.classList.toggle("open");
+}
+
+function togglePopupExprimer(){
+    let popup = document.querySelector("#popup-overlay3");
+    popup.classList.toggle("open");
+}
+
+function togglePopupDevelopper(){
+    let popup = document.querySelector("#popup-overlay4");
+    popup.classList.toggle("open");
+}
+
+function togglePopupEntreprendre(){
+    let popup = document.querySelector("#popup-overlay5");
+    popup.classList.toggle("open");
+}
